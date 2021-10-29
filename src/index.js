9bf7363e2bca9155c3a24071376dfb2be9ffec8a
happiguru <hguruman@gmail.com>
2021-10-30 02:31:39 +0400

    Add store and router to the project.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import App from './components/App';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import store from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={App} />
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
