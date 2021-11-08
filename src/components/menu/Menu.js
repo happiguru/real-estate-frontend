032ab535b110733ec507233ca462b8110b5b4642
happiguru <hguruman@gmail.com>
2021-11-08 21:23:38 +0400

    Update header margin

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -5,7 +5,7 @@ import { StyledMenu } from './Menu.styled';
 /* eslint arrow-body-style: */
 const Menu = ({ open }) => {
   return (
-    <header>
+    <header style="margin-bottom: 4750px">
       <nav>
         <StyledMenu open={open}>
           <Link to="/">Home</Link>
